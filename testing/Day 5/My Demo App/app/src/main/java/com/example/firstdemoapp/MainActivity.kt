2da5b326f82e5d5b803a29df7e7d384d1d72ae8a
uroojkhadim <uroojkhadim505@gmail.com>
2026-07-20 01:35:23 +0500

    commit

--- testing/Day 5/My Demo App/app/src/main/java/com/example/firstdemoapp/MainActivity.kt
+++ testing/Day 5/My Demo App/app/src/main/java/com/example/firstdemoapp/MainActivity.kt
@@ -1,47 +1,31 @@
-package com.example.firstdemoapp
+package com.example.myapplication
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.firstdemoapp.ui.theme.FirstDemoAppTheme
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : ComponentActivity() {
+class LoginActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            FirstDemoAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+        setContentView(R.layout.activity_login)
+
+        val email = findViewById<EditText>(R.id.etEmail)
+        val password = findViewById<EditText>(R.id.etPassword)
+        val loginButton = findViewById<Button>(R.id.btnLogin)
+
+        loginButton.setOnClickListener {
+
+            if (email.text.toString().isEmpty()) {
+                email.error = "Enter Email"
+            } else if (password.text.toString().isEmpty()) {
+                password.error = "Enter Password"
+            } else {
+                Toast.makeText(this, "Login Successful", Toast.LENGTH_SHORT).show()
             }
+
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FirstDemoAppTheme {
-        Greeting("Android")
     }
 }
